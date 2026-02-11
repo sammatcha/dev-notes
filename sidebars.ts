77c083127ea51681b79161b31a2f5c213b552e6e
Samantha <samantha@uppointinc.com>
2026-02-11 15:14:19 -0800

    changing label of sidebar

--- sidebars.ts
+++ sidebars.ts
@@ -24,7 +24,7 @@ const sidebars: SidebarsConfig = {
     'index',
     {
       type: 'category',
-      label: 'stamp-calc',
+      label: 'Stamp-Calc',
       collapsible: true,
       collapsed: false,
       items: 
